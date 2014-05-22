e1081d4eb6f17465e5976b6f25040e067880dd3a
Eloy Espinaco <eloyesp@gmail.com>
2014-05-22 17:02:25 -0300

    Use clear error messages.

--- lib/validated_accessors.rb
+++ lib/validated_accessors.rb
@@ -4,18 +4,29 @@ module ValidatedAccessors
 
   VERSION = "0.1"
 
-  def validated_accessor attribute, options
+  def validated_accessor attribute, options, &block
+    define_attribute_reader attribute
+    define_attribute_writter attribute, options, &block
+  end
 
+  private
+
+  def define_attribute_reader attribute
     define_method attribute do
       instance_variable_get "@#{ attribute }"
     end
+  end
 
+  def define_attribute_writter attribute, options, &block
     define_method "#{ attribute }=" do |value|
       value = yield value if block_given?
-      raise ArgumentError unless options[:valid].include? value
+      unless options[:valid].include? value
+        raise ArgumentError,
+          "Invalid value: #{ value } for #{ attribute }."\
+          "(Valid values are: #{ options[:valid] })"
+      end
       instance_variable_set "@#{ attribute }", value
     end
-
   end
 
 end
